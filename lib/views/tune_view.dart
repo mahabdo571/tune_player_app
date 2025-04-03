30091bc0597ca2fe59384b61c59e2cc0a3d61733
mahmoud abdo <mah.abdo571@gmail.com>
2025-04-03 15:12:36 +0200

    feat: add TuneItem widget and display color list in TuneView

--- lib/views/tune_view.dart
+++ lib/views/tune_view.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tune_player_app/widgets/tune_item.dart';
 
 class TuneView extends StatelessWidget {
   const TuneView({super.key});
+  final List<Color> tuneColors = const [
+    Colors.amber,
+    Colors.cyan,
+    Colors.red,
+    Colors.blue,
+    Colors.deepOrange,
+    Colors.teal,
+    Colors.orange,
+    Colors.indigo,
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +22,9 @@ class TuneView extends StatelessWidget {
         backgroundColor: Color(0xff253238),
         title: const Text('Flutter Tune'),
         centerTitle: true,
+      ),
+      body: Column(
+        children: tuneColors.map((color) => TuneItem(color: color)).toList(),
       ),
     );
   }
